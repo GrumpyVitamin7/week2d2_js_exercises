854cf48dd601ac9593f374235fc5eb0a6c18e4e5
GrumpyVitamin7 <theahanyuanyuan@gmail.com>
2022-10-05 17:31:21 +0100

    fixed

--- exercise.js
+++ exercise.js
@@ -58,14 +58,10 @@ console.log("the subtraction is:", theSub);
  Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
 */
 
-let name1 = "john"
-let name2 = "John"
-let isTheyAreTheSame = name1 === name2
-console.log("let's see whether they are the same:", isTheyAreTheSame);
-
-let name3 = name1 = name2.toLowerCase()
-
-console.log(name3);
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
 
 
 /* EXERCISE 8
@@ -73,27 +69,27 @@ console.log(name3);
  Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
 */
 
-let X = 9
-if(X > 9){
-    console.log("bigger than 9")
-}if(X = 8){
-    console.log("eight")
-}if(X = 7){
-    console.log("seven")
-}if(X =6){
-    console.log("six")
-}if(X = 5){
-    console.log("five")
-}if(X = 4){
-    console.log("four")
-}if(X = 3){
-    console.log("three")
-}if(X = 2){
-    console.log("two")
-}if(X = 1){
-    console.log("one")
-}if(X = 0){
-    console.log("zero")
+x = 6;
+if (x === 1) {
+  console.log("one");
+} else if (x === 2) {
+  console.log("two");
+} else if (x === 3) {
+  console.log("three");
+} else if (x === 4) {
+  console.log("four");
+} else if (x === 5) {
+  console.log("five");
+} else if (x === 6) {
+  console.log("six");
+} else if (x === 7) {
+  console.log("seven");
+} else if (x === 8) {
+  console.log("eight");
+} else if (x === 9) {
+  console.log("nine");
+} else {
+  console.log("not a valid number!");
 }
 
 
@@ -116,22 +112,27 @@ console.log(result);
  The choice should be made based on the value of another variable called isMale.
 */
 
-let gender = "male"
-let isMale = 
-(gender = "male") ? "Male" : "Female";
-console.log(isMale);
+let isMale = false;
+let gender = isMale ? "male" : "female";
+console.log(gender);
 
 /*EX 2
  Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let a = 5;
+let b = 10;
+if (a === 8 || b === 8 || a + b === 8 || a - b === 8) {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
 /*EX 3
  Create a variable and assign to it the concatenation of two strings.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let str = "Yuanyuan " + "Han";
 
 /*EX 4
  Create three variables and assign a numerical value to each one of them. 
@@ -139,35 +140,78 @@ console.log(isMale);
  Display the result in the console.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let x1 = 3;
+let y1 = 6;
+let z = -1;
+
+if (x1 > y1 && x > z) {
+  if (y > z) {
+    console.log(x1 + ", " + y1 + ", " + z);
+  } else {
+    console.log(x1 + ", " + z + ", " + y1);
+  }
+} else if (y1 > x1 && y1 > z) {
+  if (x1 > z) {
+    console.log(y1 + ", " + x1 + ", " + z);
+  } else {
+    console.log(y1 + ", " + z + ", " + x1);
+  }
+} else if (z > x1 && z > y1) {
+  if (x > y) {
+    console.log(z + ", " + x1 + ", " + y1);
+  } else {
+    console.log(z + ", " + y1 + ", " + x1);
+  }
+}
 
 /*EX 5
  Write a piece of code for finding the average of two given integers.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let d = 4;
+let h = 5;
+let average = (d + h) / 2;
+console.log(average);
 
 /*EX 6
  Write a piece of code for finding the longest of two given strings.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let str1 = "aaaa";
+let str2 = "bb";
+if (str1.length > str2.length) {
+  console.log("str1 is longer");
+} else {
+  console.log("str2 is longer");
+}
 
 /*EX 7
  Write a piece of code for checking if a given value is a integer or not.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(typeof str1);
+if (typeof d === "number") {
+  console.log(true);
+} else {
+  console.log(false);
+}
 
 /*EX 8
  Write a piece of code for calculating a certain percentage of a given number.
  (Ex.: the 20% of 400 is 80)
 */
 
-/* WRITE YOUR ANSWER HERE */
+let num = 400;
+let per = 20;
+let percentage = (400 / 100) * 20;
+console.log(percentage);
 
 /*EX 9
  Write a piece of code for checking if a given number is even or odd.
 */
 
-/* WRITE YOUR ANSWER HERE */
+if (num % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
